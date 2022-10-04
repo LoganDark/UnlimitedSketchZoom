fd950dea7ffb167c87161a9dc0929327e7c4c0e4
LoganDark <git@logandark.mozmail.com>
2022-10-04 12:23:54 -0700

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Reflection;
+
+using UnityModManagerNet;
+using HarmonyLib;
+
+namespace UnlimitedSketchZoom {
+#if DEBUG
+	[EnableReloading]
+#endif
+	public static class Main {
+		static Harmony harmony;
+		static UnityModManager.ModEntry lastEntry;
+
+		public static bool Load(UnityModManager.ModEntry entry) {
+			harmony = new Harmony(entry.Info.Id);
+
+			entry.OnToggle = OnToggle;
+#if DEBUG
+			entry.OnUnload = OnUnload;
+#endif
+			lastEntry = entry;
+
+			return true;
+		}
+
+		static bool OnToggle(UnityModManager.ModEntry entry, bool active) {
+			if (active) {
+				harmony.PatchAll(Assembly.GetExecutingAssembly());
+			} else {
+				harmony.UnpatchAll(entry.Info.Id);
+			}
+
+			return true;
+		}
+
+#if DEBUG
+		static bool OnUnload(UnityModManager.ModEntry entry) {
+			lastEntry = null;
+			return true;
+		}
+#endif
+
+		[HarmonyPatch(typeof(Visor.ProcessorUI), nameof(Visor.ProcessorUI.EnterZoomView))]
+		class Patch {
+			static MethodInfo rewired_getAxis = AccessTools.Method(typeof(Rewired.Player), nameof(Rewired.Player.GetAxis), new[] { typeof(string) });
+
+			static FieldInfo _canClose = AccessTools.Field(typeof(Visor.ProcessorUI), "_canClose");
+			static FieldInfo _input = AccessTools.Field(typeof(Visor.ProcessorUI), "_input");
+			static FieldInfo _sketchView = AccessTools.Field(typeof(Visor.ProcessorUI), "_sketchView");
+
+			static IEnumerator EnterZoomView(Visor.ProcessorUI instance) {
+				_canClose.SetValue(instance, false);
+				
+				try {
+					float delta = ((Rewired.Player) _input.GetValue(instance)).GetAxis("ScaleComponent");
+
+					if (delta > 0f) {
+						((Visor.SketchView) _sketchView.GetValue(instance)).zoomFactor /= 2f;
+					} else if (delta < 0f) {
+						((Visor.SketchView) _sketchView.GetValue(instance)).zoomFactor *= 2f;
+					}
+				} finally {
+					_canClose.SetValue(instance, true);
+				}
+
+				yield break;
+			}
+
+			public static bool Prefix(Visor.ProcessorUI __instance, ref IEnumerator __result) {
+				__result = EnterZoomView(__instance);
+				return false;
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# UnlimitedSketchZoom
+
+A mod for Plasma Demo that lets you zoom however far you want in a sketch.
+
+## Installation
+
+1. Grab a zip from
+   [GitHub Releases](https://github.com/LoganDark/UnlimitedSketchZoom/releases)
+
+2. Install UnityModManager on Plasma Demo as instructed
+   [here](https://github.com/LoganDark/PlasmaDemoModTemplate)
+
+3. Go to the 'Mods' tab of UnityModManager, and drag the zip into the "Drop zip
+   files here" area
