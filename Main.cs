20d2ee31a354cfe1c62ce6c3c190ec00dc0b8b98
LoganDark <git@logandark.mozmail.com>
2022-10-04 13:16:47 -0700

    Add preliminary support for speed factor changing
    Could be useful

--- Main.cs
+++ Main.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 
 using UnityModManagerNet;
 using HarmonyLib;
+using UnityEngine;
 
 namespace UnlimitedSketchZoom {
 #if DEBUG
@@ -10,11 +11,24 @@ namespace UnlimitedSketchZoom {
 #endif
 	public static class Main {
 		static Harmony harmony;
+		static float factor = 2f;
 
 		public static bool Load(UnityModManager.ModEntry entry) {
 			harmony = new Harmony(entry.Info.Id);
 			entry.OnToggle = OnToggle;
+			entry.OnGUI = OnGUI;
+			entry.OnSaveGUI = OnSaveGUI;
 			return true;
+		}
+
+		private static void OnGUI(UnityModManager.ModEntry entry) {
+			GUILayout.BeginHorizontal();
+			UnityModManager.UI.DrawFloatField(ref factor, "Factor");
+			GUILayout.Label("Controls how much you zoom with each click of the scroll wheel");
+			GUILayout.EndHorizontal();
+		}
+		private static void OnSaveGUI(UnityModManager.ModEntry entry) {
+			UnityModManager.Logger.Log("haha not saving lmao");
 		}
 
 		static bool OnToggle(UnityModManager.ModEntry entry, bool active) {
@@ -42,9 +56,9 @@ namespace UnlimitedSketchZoom {
 					float delta = ((Rewired.Player) _input.GetValue(instance)).GetAxis("ScaleComponent");
 
 					if (delta > 0f) {
-						((Visor.SketchView) _sketchView.GetValue(instance)).zoomFactor /= 2f;
+						((Visor.SketchView) _sketchView.GetValue(instance)).zoomFactor /= factor;
 					} else if (delta < 0f) {
-						((Visor.SketchView) _sketchView.GetValue(instance)).zoomFactor *= 2f;
+						((Visor.SketchView) _sketchView.GetValue(instance)).zoomFactor *= factor;
 					}
 				} finally {
 					_canClose.SetValue(instance, true);
